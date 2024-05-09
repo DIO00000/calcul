0599890b10fd98a4a99adf6661d17792d6d5c829
Hamza Bettal <hbettal@e2r9p4.1337.ma>
2024-05-09 22:46:57 +0100

    2+2*2??

--- calcul/calculation.js
+++ calcul/calculation.js
@@ -1,15 +1,27 @@
 
 function strserch(input, c)
 {
-	i = -1;
-	while (input[++i])
+	if (typeof input === 'undefined')
+        return false;
+	i = 0;
+	while (input[i])
 	{
 		if (input[i] === c)
-		return (true);
+			return (true);
+		i++;
 	}
 	return (false);
 }
 
+function calculator(a, sign, b)
+{
+	sum = 0;
+	if (sign === '*')
+		sum = a * b;
+	else if (sign === '/')
+		sum = a / b;
+	return sum;
+}
 
 function power_first(input)
 {
@@ -19,37 +31,40 @@ function power_first(input)
 	sume = 0;
 	j = 0;
 	sign = 0;
-	split = input.split('+');
+	input = String(input);
+	split = input.split(/([+-])/); // 5+5*3+2 == 5 + 5*3 + 2
 	while (split[++i])
 	{
-		if (strserch(input, '/') == true || strserch(input, '*') == true)
+		j = 0;
+		if (strserch(split[i], '/') == true || strserch(split[i], '*') == true)
 		{
-			while (input[i] >= 0 && input[i] <= 9)
+			while (split[i][j] >= 0 && split[i][j] <= 9)
 			{
-				num = num + input[i] - '0';
-				i++;
+				num = num + split[i][j] - '0';
+				j++;
 			}
-			sign = input[i++];
-			while (input[i] < '0' || input > '9')
+			sign = split[i][j++];
+			while (split[i][j] < '0' || split[i][j] > '9')
 			{
-				if (input[i] === '-')
+				if (split[i][j] === '-')
 					sign = '-';
-				else if (input[i] === '*' || input[i] === '/')
+				else if (split[i][j] === '*' || split[i][j] === '/')
 					return ("not valid yet");
-				i++;
+				j++;
 			}
-			while (input[i] >= 0 && input[i] <= 9)
+			while (split[i][j] >= 0 && split[i][j] <= 9)
 			{
-				num2 = num2 + input[i] - '0';
-				i++
+				num2 = num2 + split[i][j] - '0';
+				j++;
 			}
 			if (sign === '*')
 				sume = num * num1;
 			else if (sign === '/')
 				sume = num / num1;
-			input = input.replace(split[i], sume);
 		}
 	}
+	input = input.replace(split[i], String(sume));
+	console.log(input);
 	return input
 }
 var some = 0;
@@ -68,9 +83,9 @@ function parser(input)
 	sign = 0;
 	i = 0;
 	
-	while (strserch(input, '*') == true || strserch(input, '/') == true)
-		input = power_first(sign, num);
-	while (input[i] >= '0' && input <= '9')
+	if (strserch(input, '*') == true || strserch(input, '/') == true)
+		input = power_first(input);
+	while (input[i] >= '0' && input[i] <= '9')
 	{
 		some = some + input[i] - '0';
 		i++;
@@ -101,10 +116,11 @@ function print_calul(value)
 {
     output = document.getElementById('out');
     if (value === 'c')
-        output.value = '0';
+        output.value = '';
     else if (value === '=')
 	{
         output.value = parser(output.value);
+		some = 0;
     }
     else
         output.value += value;
